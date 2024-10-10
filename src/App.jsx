083e2886dc0da69c7266523583c044875a4f6477
samadovoybek758 <oybek22samadov@gmail.com>
2024-10-10 23:03:43 +0500

    filterda ozroq muammo bulyapti

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import React, { useEffect, useRef, useState } from "react";
+import './App.css'
 import { PaginationControl } from "react-bootstrap-pagination-control";
+import { ssrImportMetaKey } from "vite/runtime";
+import {DNA} from 'react-loader-spinner'
 
 const App = () => {
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(20);
   const [skib, setSkib] = useState();
-  const [loader, setLoader] = useState(true)
+  const [loader, setLoader] = useState(false)
+  
 
   const [data, setData] = useState([]);
   const genderRef = useRef();
   const majorRef = useRef();
+  const formRef = useRef()
 
   useEffect(() => {
     const newSkip = (page - 1) * limit;
@@ -20,7 +25,7 @@ const App = () => {
 
 
   useEffect(() => {
-    setLoader(false)
+    setLoader(true)
     fetch(
       `https://json-api.uz/api/project/11-dars/developers?skip=${skib}&limit=${limit}`
     )
@@ -32,7 +37,7 @@ const App = () => {
         console.log(err);
       })
       .finally(()=>{
-        setLoader(true)
+        setLoader(false)
       })
   }, [skib]);
 
@@ -43,27 +48,36 @@ const App = () => {
     let copied = [...data];
     if (genderRef.current.value) {
       copied = copied.filter((value) => {
-        return value.gender == genderRef.current.value;
+        // console.log(value);
+        return value.gender == genderRef.current.value
       });
     }
     if (majorRef.current.value) {
       copied = copied.filter((value) => {
-        return value.major == majorRef.current.value;
+        return value.major == majorRef.current.value
       });
     }
-    console.log(copied);
-    setData(copied);
+    if (copied.length === 0) {
+      console.log("malumot yuq")
+    } else {
+      setData(copied);
+    }
+   
+    genderRef.current.value = "Gender"; 
+    majorRef.current.value = "Major";
+
   }
   function add_data(e) {
     e.preventDefault()
     setLimit((add) => add + 10); 
   }
   return (
-    <>
-      <form className="border border-solid border-gray-800 mx-auto p-3 mt-8 max-w-96 flex gap-4 flex-col">
+    <div className="continer py-8">
+      
+      <form ref={formRef} className="border border-solid border-gray-800 rounded-md mx-auto p-3  max-w-96 flex gap-4 flex-col">
         <select
           ref={genderRef}
-          className="border border-solid border-blue-600 rounded-md text-gray-500 px-3 py-1"
+          className="border border-solid border-blue-600 rounded-md text-white px-3 py-1 bg-sky-400"
         >
           <option disabled value="Gender">
             Gender
@@ -73,7 +87,7 @@ const App = () => {
         </select>
         <select
           ref={majorRef}
-          className="border border-solid border-blue-600 rounded-md text-gray-500 px-3 py-1"
+          className="border border-solid border-blue-600 rounded-md text-white px-3 py-1 bg-sky-400"
         >
           <option disabled value="Major">
             Major
@@ -96,21 +110,27 @@ const App = () => {
           Filter
         </button>
       </form>
-
+      <div className="w-52 mt-10 mx-auto ">
+      {
+           loader && <DNA height="96"width="96" className="mx-auto" />  
+      }
+      </div>
       <div className="w-[1200px] mx-auto flex flex-wrap gap-3 mt-20 mb-11">
+      
         {data.length > 0 &&
           data.map((developer) => (
             <div
               key={developer.id}
-              className="w-52 bg-slate-500 p-3 rounded-md text-white "
+              className="w-52 bg-transparent border border-solid border-blue-500 p-3 rounded-md text-white "
             >
+         
               <h1>Name: {developer.fullName}</h1>
               <h1>Major: {developer.major}</h1>
               <h1>Age: {developer.age}</h1>
               <h1>Gender: {developer.gender}</h1>
             </div>
           ))}
-          <button onClick={add_data} className="max-w-80 py-2 bg-stone-600 text-white rounded-md border-none mx-auto px-24">Ko'proq</button>
+          <button onClick={add_data} className="mt-10 max-w-80 py-2 bg-violet-500 text-white rounded-md border-none mx-auto px-24">Ko'proq</button>
       </div>
       
       <PaginationControl
@@ -123,7 +143,7 @@ const App = () => {
         }}
         ellipsis={1}
       />
-    </>
+    </div>
   );
 };
 export default App;
